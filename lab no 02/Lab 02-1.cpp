2b85ac69485c6e5a127a03dd8eb05cd443e6112a
Ammar-Yasir-a11y <ammar.kharmang@gmail.com>
2025-12-03 19:31:26 +0500

    Add files via upload

---
+++ lab no 02/Lab 02-1.cpp
@@ -0,0 +1,16 @@
+//          TASK 01
+#include<iostream>
+//using namespace std;
+//int main(){
+//	cout<<"Item\tPrice\tQuality\nPen\t$1\t10\nPencil\t$2\t5\nEraser\t$0.5\t20 ";
+//	return 0;
+//          }
+
+//          TASK 02
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"She said,\"Let's learn C++!\"\nTo start, save your file in C:\\C++_Projects\\Lab1\\\nGood luck! ";
+	return 0;
+          }
+          
