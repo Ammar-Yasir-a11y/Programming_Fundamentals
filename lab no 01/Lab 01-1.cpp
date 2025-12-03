893fe42823915356e04327307b2f0fcd39e104a4
Ammar-Yasir-a11y <ammar.kharmang@gmail.com>
2025-12-03 19:29:51 +0500

    Add files via upload

---
+++ lab no 01/Lab 01-1.cpp
@@ -0,0 +1,23 @@
+//          TASK 01
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	cout<<"Wellcome to the C++ lap!"<<endl;
+//	cout<<"You will learn basic output today"<<endl;
+//	cout<<"Let's start with simple messsages";
+//	return 0;
+//         }
+
+
+ //        TASK 02
+#include<iostream>
+using namespace std;
+int main(){
+     cout<<"*\n* *\n* * *\n* * * *\n* * * * *";
+          }
+
+
+
+
+
+
