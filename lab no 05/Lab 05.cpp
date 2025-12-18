f3ab30d263d250d11dbf081b662606fc2f3ec147
Ammar-Yasir-a11y <ammar.kharmang@gmail.com>
2025-12-18 22:28:39 +0500

    Add files via upload

---
+++ lab no 05/Lab 05.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	int markes;
+//	cout<<"enter the student markes ";
+//	cin>>markes;
+//	if(markes>=90){
+//		cout<<"A+";
+//	}
+//	if(markes>=80&&markes<=89){
+//		cout<<"A";
+//	}
+//	if(markes>=70&&markes<=79){
+//		cout<<"B";
+//	}
+//	if(markes>=60&&markes<=69){
+//		cout<<"C";
+//	}
+//	if(markes<60){
+//		cout<<"Fail";
+//	}
+//} 
+
+//char colour;
+//cout<<"enter the character ";
+//cin>>colour;
+//if(colour=='R'){
+//		cout<<"stop";
+//	}
+//	if(colour=='G'){
+//		cout<<"Go";
+//	}
+//	if(colour=='Y'){
+//		cout<<"slow down";
+//	}
+//	int account_balance,withdrawal_amount;  incomplete
+//	cout<<"enter the account balance ";
+//	cin>>account_balance;
+//	cout<<"enter the withdrawal amount ";
+//	cin>>withdrawal_amount;
+//	if(withdrawal_amount<=account_balance){
+//		cout<<"the remain balance is ";
+//	}
+//	else{
+//			cout<<"insufficient balance";
+//		
+//	}
+
+}
