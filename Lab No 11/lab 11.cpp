c75f2a0f51b4c84151f0cabcd5e2b19a5e00fd84
Ammar-Yasir-a11y <ammar.kharmang@gmail.com>
2026-01-13 14:29:31 +0500

    Add files via upload

---
+++ Lab No 11/lab 11.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+        //Task No 01	
+//int array[5]={12,23,45,56,56};
+//cout<<"The element of an array is: \n";
+//for(int i=0;i<=4;i++){
+//	cout<<array[i]<<endl;
+//}
+	//Task No 02  Revers of an array
+//int array[5]={12,23,45,56,56};
+//cout<<"The element of an array is: \n";
+//for(int i=4;i>=0;i--){
+//	cout<<array[i]<<endl;
+//		}
+	//Task No 03
+//int arr[5],i,sum=0;
+//float avg;
+//cout<<"enter the element of an arrary \n";
+//for(i=0;i<=4;i++){
+//cin>>arr[i];int
+//sum=sum+arr[i];
+//}
+//avg=sum/5;
+//cout<<"The sum is = "<<sum<<endl;
+//cout<<"The average is = "<<avg;		
+		//Task No 04
+		int array[]={12,23,45,56,56};
+	    int length=sizeof(array)/sizeof(array[0]);
+	    cout<<"The length of an array is = "<<length;
+	    
+			}
+		
+		
+	
