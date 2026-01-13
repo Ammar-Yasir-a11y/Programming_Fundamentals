82b5b8a3dddd3ce83369afc66a551f0702d93cf8
Ammar-Yasir-a11y <ammar.kharmang@gmail.com>
2026-01-13 14:26:45 +0500

    Add files via upload

---
+++ Lab No 10/Lab No 10.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	 Task No 01
+//	int N;
+//	cout<<"enter the number: ";
+//	cin>>N;
+//	for(int i=1;i<=N;++i){
+//		cout<<i;
+//	}
+//	 Task No 02
+//int num;
+//cout<<"enter the number: ";
+//cin>>num;
+//cout<<"the multication table of "<<num<<endl;
+//for(int i=1;i<=num;i++){
+//	cout<<num<<"*"<<i<<"="<<num*i<<endl;
+//}
+//	 Task No 03
+//int N,sum=0;
+//cout<<"Enter the number ";
+//cin>>N;
+//for(int i=1;i<=N;i++){
+//	sum=sum+i;
+//	}
+//cout<<"sum is = "<<sum;
+//	 Task No 04
+int i,fac=1,n;
+cout<<"Enter the number for factorial = ";
+cin>>n;
+for(i=1;i<=n;i++){
+	fac=fac*i;
+}
+cout<<"fsctorial of "<<n<<" is = "<<fac;
+}
